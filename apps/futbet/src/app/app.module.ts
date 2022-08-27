203addc1c1254a7d9345d2cbabf325f7fa99a4f9
Luis Castro <luish.castroc@gmail.com>
2022-08-27 11:21:26 -0400

    initial commit

--- apps/futbet/src/app/app.module.ts
+++ apps/futbet/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideDatabase(() => getDatabase()), provideFirestore(() => getFirestore())],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/futbet/src/environments/environment.ts
+++ apps/futbet/src/environments/environment.ts
@@ -3,6 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'futbet-3921b',
+    appId: '1:775954989186:web:5a19d6b435882bdcc4b49b',
+    databaseURL: 'https://futbet-3921b-default-rtdb.firebaseio.com',
+    storageBucket: 'futbet-3921b.appspot.com',
+    locationId: 'us-east1',
+    apiKey: 'AIzaSyAdVJxWtaIDL2BEDqVrzu_hi3Y2YOPJnCA',
+    authDomain: 'futbet-3921b.firebaseapp.com',
+    messagingSenderId: '775954989186',
+    measurementId: 'G-T7W1TBNX8E',
+  },
   production: false,
 };
 
